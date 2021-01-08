a555df74b84e5ed6379e22ffacb9f2676cc45d27
Pedro Albuquerque Santos <petersaints@gmail.com>
2021-01-08 20:52:49 +0000

    - Some tweaks
    - Package updates

--- src/config/yanuxCoordinator.js
+++ src/config/yanuxCoordinator.js
@@ -1,25 +1,25 @@
 const config = {
     components_restrictions: {
         Note: {
-            showByDefault: true,
+            //showByDefault: true,
             display: {
                 operator: "AND",
                 values: {
-                    virtualResolution: { operator: ">=", value: [1280, null] },
+                    virtualResolution: { operator: ">=", value: [1280, null], enforce: false },
                     size: { operator: ">=", value: [200, null], enforce: false }
                 },
             },
             speakers: { channels: { operator: ">=", value: 1 } },
-            input: { operator: "OR", values: ["mouse", "touchscreen"], enforce: false },
+            input: { operator: "OR", values: ["mouse", "touchscreen"] },
         },
         Edit: {
-            showByDefault: true,
+            //showByDefault: true,
             type: { value: "smartphone", enforce: false },
             display: true,
             input: { operator: "OR", values: ["mouse", "touchscreen"] },
         },
         List: {
-            showByDefault: true,
+            //showByDefault: true,
             type: { value: "smartphone", enforce: false },
             display: true,
             input: { operator: "OR", values: ["mouse", "touchscreen"] },
